273e4c39d5208fbb5b6ab76bc5945659954610ba
kssytsrk <kassy@dismail.de>
2021-01-28 01:15:37 +0200

    stopped js from erroring out with dumb approach, will fix that later

--- percentage.lisp
+++ percentage.lisp
@@ -4,11 +4,29 @@
   (defun percentage ()
     (let* ((height-of-window (ps:@ window inner-height))
            (content-scrolled (ps:@ window page-y-offset))
-           (body-height (ps:chain window
-                                  document
-                                  (get-elements-by-tag-name "body")
-                                  0
-                                  offset-height))
+           (body-height (if (not (or (eql window undefined)
+                                     (eql (ps:@ window document) undefined)
+                                     (eql (ps:chain window
+                                                    document
+                                                    (get-elements-by-tag-name "body"))
+                                          undefined)
+                                     (eql (ps:chain window
+                                                    document
+                                                    (get-elements-by-tag-name "body")
+                                                    0)
+                                          undefined)
+                                     (eql (ps:chain window
+                                                    document
+                                                    (get-elements-by-tag-name "body")
+                                                    0
+                                                    offset-height)
+                                          undefined)))
+                          (ps:chain window
+                                    document
+                                    (get-elements-by-tag-name "body")
+                                    0
+                                    offset-height)
+                          0))
            (total (- body-height height-of-window))
            (prc (* (/ content-scrolled total) 100)))
       (if (> prc 100)
